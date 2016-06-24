fc83061a60d1134aa99f1cc69ccdfe85663f336c
impaladove <djhawro@hotmail.co.uk>
2016-06-24 11:39:09 +0100

    master

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ function increment() {
 
 		setTimeout(function() {
 			time++;
-			var min = Math.floor(time/10/60);
-			var sec = Math.floor(time/10 % 60);
+			var min = Math.floor(time/100/60);
+			var sec = Math.floor(time/100 % 60);
 			var hour = Math.floor(time/10/60/60);
-			var tenth = time%10;
+			var tenth = Math.floor(time%100);
 
 			if(min<10){
 				min = "0" + min;
@@ -41,13 +41,13 @@ function increment() {
 				sec = "0" + sec;
 			}
 			if(tenth<10){
-				tenth = tenth + "0";
+				tenth = "0" + tenth;
 			}
 			
-			
+
 
 			document.getElementById("output").innerHTML = hour + ": " + min + ": " + sec + ": " + tenth;
 			increment();
-	}, 100);
+	}, 10);
 	}
 };
